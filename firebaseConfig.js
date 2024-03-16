1a6abe34dff997032974586b6a74f603125bd074
Edu Bravo <eduardobravopires@gmail.com>
2024-03-16 16:44:36 -0300

    feat: add env variables

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBeKA1KxAl9czuqe3XBqHASezD30Dyp2c",
-  authDomain: "geometryio.firebaseapp.com",
-  projectId: "geometryio",
-  storageBucket: "geometryio.appspot.com",
-  messagingSenderId: "958876768162",
-  appId: "1:958876768162:web:01515b4e6b398bce4544fb"
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
