afa75f6f1325dbed3019052886f6e2774d3e94d0
Edu Bravo <eduardobravopires@gmail.com>
2024-03-16 16:34:23 -0300

    initial commit

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBeKA1KxAl9czuqe3XBqHASezD30Dyp2c",
+  authDomain: "geometryio.firebaseapp.com",
+  projectId: "geometryio",
+  storageBucket: "geometryio.appspot.com",
+  messagingSenderId: "958876768162",
+  appId: "1:958876768162:web:01515b4e6b398bce4544fb"
+};
+
+const app = initializeApp(firebaseConfig);
