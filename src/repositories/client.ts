892a5fa2115babed4f76deca234be702903e2b6e
Edu Bravo <eduardobravopires@gmail.com>
2024-03-18 15:21:34 -0300

    feat: initialize firebase apps

--- src/repositories/client.ts
+++ src/repositories/client.ts
@@ -1,4 +1,7 @@
-import { initializeApp } from "firebase/app";
+import { FirebaseApp, getApp, getApps, initializeApp } from "firebase/app";
+import { getReactNativePersistence, initializeAuth, getAuth, Auth } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_API_KEY,
@@ -6,9 +9,26 @@ const firebaseConfig = {
   projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
   storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_APP_ID
+  appId: process.env.EXPO_PUBLIC_APP_ID,
+  databaseURL: process.env.EXPO_PUBLIC_DATABASE_URL
 };
 
-const app = initializeApp(firebaseConfig);
+let app: FirebaseApp, auth: Auth;
 
-export default app
+if (!getApps().length) {
+  try {
+    app = initializeApp(firebaseConfig);
+    auth = initializeAuth(app, {
+      persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+    });
+  } catch (error) {
+    console.log("Error initializing app: " + error);
+  }
+} else {
+  app = getApp();
+  auth = getAuth(app);
+}
+
+const database = getDatabase(app);
+
+export {app, auth, database}
