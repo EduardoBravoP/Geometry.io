88eefd98ac9090215a2a21a8a1690b806048b39f
Edu Bravo <eduardobravopires@gmail.com>
2024-03-17 23:47:54 -0300

    feat: created app themes

---
+++ src/themes/index.ts
@@ -0,0 +1,26 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+
+    BACKGROUND: '#22272C',
+    
+    GRAY_500: '#383D41',
+    GRAY_400: '#7A7D80',
+    GRAY_100: '#D3D3D3',
+
+    RED_500: '#DA0000',
+
+    BLUE_500: '#3983DA'
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Montserrat_400Regular',
+    MEDIUM: 'Montserrat_500Medium',
+    SEMI_BOLD: 'Montserrat_600SemiBold'
+  },
+  FONT_SIZE: {
+    XS: '12px',
+    SM: '14px',
+    MD: '16px',
+    XL: '32px'
+  }
+}
