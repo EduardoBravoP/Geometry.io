2c535748db9d74b34b30821e1fc3c4f214abe2bb
Edu Bravo <eduardobravopires@gmail.com>
2024-03-18 15:21:15 -0300

    feat: add StatusBar on SignIn

--- src/pages/SignIn/view.tsx
+++ src/pages/SignIn/view.tsx
@@ -5,7 +5,7 @@ import Icon from '@expo/vector-icons/MaterialIcons'
 import themes from "../../themes"
 import useSignInViewModel from "./view.model"
 import { ActivityIndicator } from "react-native"
-import { useNavigation } from "@react-navigation/native"
+import { StatusBar } from "expo-status-bar"
 
 const SignInView = () => {
   const {
@@ -18,48 +18,51 @@ const SignInView = () => {
   } = useSignInViewModel()
 
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: themes.COLORS.BACKGROUND}}>
-      <Container keyboardShouldPersistTaps="handled">
-        <LogoWrapper>
-          <LogoText>Geometry.</LogoText>
-          <LogoText red>io</LogoText>
-        </LogoWrapper>
+    <>
+      <StatusBar style="light" backgroundColor={themes.COLORS.BACKGROUND} translucent />
+      <SafeAreaView style={{flex: 1, backgroundColor: themes.COLORS.BACKGROUND}}>
+        <Container keyboardShouldPersistTaps="handled">
+          <LogoWrapper>
+            <LogoText>Geometry.</LogoText>
+            <LogoText red>io</LogoText>
+          </LogoWrapper>
 
-        <Title>Entrar</Title>
-        <Description>Bem vindo de volta!</Description>
+          <Title>Entrar</Title>
+          <Description>Bem vindo de volta!</Description>
 
-        <InputsWrapper>
-          <Input
-            label="E-mail"
-            placeholder="Insira seu e-mail" 
-            value={email}
-            onChangeText={setEmail}
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
-          <Input
-            label="Senha"
-            placeholder="Insira sua senha"
-            value={password}
-            onChangeText={setPassword}
-            secretInput
-          />
-        </InputsWrapper>
+          <InputsWrapper>
+            <Input
+              label="E-mail"
+              placeholder="Insira seu e-mail" 
+              value={email}
+              onChangeText={setEmail}
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+            <Input
+              label="Senha"
+              placeholder="Insira sua senha"
+              value={password}
+              onChangeText={setPassword}
+              secretInput
+            />
+          </InputsWrapper>
 
-        <SignInButton onPress={onSubmit} disabled={isLoading}>
-          {isLoading ? (
-            <LoadingContainer>
-              <ActivityIndicator size={24} color={themes.COLORS.WHITE} />
-            </LoadingContainer>
-          ) : (
-            <>
-              <SignInButtonText>Entrar</SignInButtonText>
-              <Icon name="arrow-forward" color={themes.COLORS.WHITE} size={24} />
-            </>
-          )}
-        </SignInButton>
-      </Container>
-    </SafeAreaView>
+          <SignInButton onPress={onSubmit} disabled={isLoading}>
+            {isLoading ? (
+              <LoadingContainer>
+                <ActivityIndicator size={24} color={themes.COLORS.WHITE} />
+              </LoadingContainer>
+            ) : (
+              <>
+                <SignInButtonText>Entrar</SignInButtonText>
+                <Icon name="arrow-forward" color={themes.COLORS.WHITE} size={24} />
+              </>
+            )}
+          </SignInButton>
+        </Container>
+      </SafeAreaView>
+    </>
   )
 }
 
