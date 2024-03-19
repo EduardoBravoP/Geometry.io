10e58ec563b814815e71eb40d17d8f00e1254cac
Edu Bravo <eduardobravopires@gmail.com>
2024-03-18 22:36:45 -0300

    fix: removed comment

--- src/pages/Render/view.model.tsx
+++ src/pages/Render/view.model.tsx
@@ -13,12 +13,6 @@ const useRenderViewModel = () => {
     if (userSettings) {
       setObjectSettings(userSettings)
     } else {
-      // await saveUserSettings(user.uid, {
-      //   object1: {shape:"cube",color:"#FF00FF",rotation:90},
-      //   object2: {shape:"cone",color:"#2fff00",rotation:45},
-      //   object3: {shape:"dodecahedron",color:"#FFFF00",rotation:0}
-      // })
-
       setObjectSettings(getObjectsDefaultConfig())
     }
   }
